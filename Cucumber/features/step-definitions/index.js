20453c36c9cb8964958a73b56c21e295a7546d45
Holman Garcia <ha.garcia@uniandes.edu.co>
2020-02-26 19:24:10 -0500

    Register

--- Cucumber/features/step-definitions/index.js
+++ Cucumber/features/step-definitions/index.js
@@ -62,5 +62,49 @@ defineSupportCode(({Given, When, Then}) => {
 		var buttonCta = browser.element('#cuenta');
 		expect(buttonCta).to.exist;
 	});
+	
+	When(/^I fill with my (.*), (.*), (.*), selected a (.*), fill my (.*) and accept the (.*) and conditions$/, 
+        (name, lastname, email, department, password, terms) => {
+            var cajaSignUp = browser.element('.cajaSignUp');
+            cajaSignUp.element('input[name="nombre"]').click().keys(name);
+            cajaSignUp.element('input[name="apellido"]').click().keys(lastname);
+            cajaSignUp.element('input[name="correo"]').click().keys(email);
+            cajaSignUp.element('input[name="password"]').click().keys(password);
+            if(department !== "")
+                cajaSignUp.element('select[name="idDepartamento"]').selectByValue('3');
+            if(terms !== "")
+                cajaSignUp.element('input[name="acepta"]').click();
+    });
+
+    When('I try to register', () => {
+        var cajaSignUp = browser.element('.cajaSignUp');
+        cajaSignUp.element('button=Registrarse').click();
+    });
+
+    Then(/^I expect do not see the errors : (.*), (.*) and (.*)$/, 
+        (error_email, error_password, error_terms) => {
+            if(error_email !== "" && error_password !== "" && error_terms !== "") {
+                browser.waitForVisible('.aviso.alert.alert-danger', 5000);
+                var alertText = browser.element('.aviso.alert.alert-danger').getText();
+                var msgErrors = [
+                    error_email, 
+                    error_password, 
+                    error_terms
+                ];
+                //Saber si alguno de los siguientes errores es mostrando...
+                var showError = false;
+                var numBerError = 0;
+                for(var i = 0; i < msgErrors.length; i++) {
+                    if(msgErrors[i] === alertText) {
+                        showError = true;
+                        numBerError = i;
+                        break;
+                    }
+                }
+                if(showError) {
+                    expect(alertText).to.include(msgErrors[numBerError]);
+                }
+            }
+    });
 });
 
