aa0ecc15049b97e145d984eee9288c1361092517
Holman Garcia <ha.garcia@uniandes.edu.co>
2020-02-27 21:08:46 -0500

    register

--- Cucumber/features/step-definitions/index.js
+++ Cucumber/features/step-definitions/index.js
@@ -65,7 +65,7 @@ defineSupportCode(({Given, When, Then}) => {
 		}
 		
 		var selectPrograma = cajaSignUp.$('select[name="idPrograma"]');
-		selectPrograma.selectByValue(program);
+		selectPrograma.selectByVisibleText(program);
 	});
 
 	When(/^I set the password (.*) and sign up$/, (password) => {
